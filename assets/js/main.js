2325858d6cd1b503378daff68090a003fadd91f5
SadmanSalim <sadmanmd225@gmail.com>
2024-12-02 12:52:47 +0600

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 $(".mobSearch").click(function(){
     $(".searchBoxMobile input").addClass('active');
+    $(".searchBoxMobile button").addClass('active');
     $(".background").addClass('active');
+});
+$(".background").click(function(){
+    $(".searchBoxMobile input").removeClass('active');
+    $(".searchBoxMobile button").removeClass('active');
+    $(".background").removeClass('active');
 })
