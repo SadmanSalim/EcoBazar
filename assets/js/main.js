b148f216282956a5965c6aedfbff3441b7b79780
SadmanSalim <sadmanmd225@gmail.com>
2024-12-04 01:53:56 +0600

    fix bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,75 +1,45 @@
 $(document).ready(function () {
+    // Toggle submenu and icon
     $('.shopCollapse').click(function () {
-      let submenu = $(this).find('.shopOpen');
-      submenu.toggleClass('active');
-  
-      let iconElement = $(this).find('iconify-icon');
-      let currentIcon = iconElement.attr('icon');
-  
-  
-      // Toggle the icon
-      if (currentIcon === 'ep:arrow-down-bold') {
-        iconElement.attr('icon', 'ep:arrow-right-bold');
-      } else {
-        iconElement.attr('icon', 'ep:arrow-down-bold');
-      }
+        let submenu = $(this).find('.shopOpen');
+        let iconElement = $(this).find('iconify-icon');
+        submenu.toggleClass('active');
+        iconElement.attr(
+            'icon',
+            iconElement.attr('icon') === 'ep:arrow-down-bold' 
+            ? 'ep:arrow-right-bold' 
+            : 'ep:arrow-down-bold'
+        );
     });
-    // password Toggler
+
+    // Password visibility toggle
     $('.pasToggl').click(function () {
-      // Find the input field within the same parent container as the clicked icon
-      var inputField = $(this).siblings('input');
-  
-      // Toggle the input type
-      var type = inputField.attr('type') === 'password' ? 'text' : 'password';
-      inputField.attr('type', type);
-  
-      // Toggle the icon
-      var icon = type === 'password' ? 'ion:eye-off-outline' : 'ion:eye-outline';
-      $(this).find('iconify-icon').attr('icon', icon);
+        let inputField = $(this).siblings('input');
+        let isPassword = inputField.attr('type') === 'password';
+        inputField.attr('type', isPassword ? 'text' : 'password');
+        $(this).find('iconify-icon').attr(
+            'icon', 
+            isPassword ? 'ion:eye-outline' : 'ion:eye-off-outline'
+        );
     });
+
+    // Switch between Register and Sign-In
     $('.RegisterBtn').click(function () {
-      $('.Register').removeClass('d-none');
-      $('.signIn').addClass('d-none');
+        $('.Register').removeClass('d-none');
+        $('.signIn').addClass('d-none');
     });
     $('.signInBtn').click(function () {
-      $('.Register').addClass('d-none');
-      $('.signIn').removeClass('d-none');
+        $('.Register').addClass('d-none');
+        $('.signIn').removeClass('d-none');
     });
-          // Toggle the icon
-          if (currentIcon === 'ep:arrow-down-bold') {
-              iconElement.attr('icon', 'ep:arrow-right-bold');
-          } else {
-              iconElement.attr('icon', 'ep:arrow-down-bold');
-          }
-      $('.pasToggl').click(function () {
-          // Find the input field within the same parent container as the clicked icon
-          var inputField = $(this).siblings('input');
-      
-          // Toggle the input type
-          var type = inputField.attr('type') === 'password' ? 'text' : 'password';
-          inputField.attr('type', type);
-      
-          // Toggle the icon
-          var icon = type === 'password' ? 'ion:eye-off-outline' : 'ion:eye-outline';
-          $(this).find('iconify-icon').attr('icon', icon);
-        });
-      
-        $('.RegisterBtn').click(function () {
-          $('.Register').removeClass('d-none');
-          $('.signIn').addClass('d-none');
-        });
-        $('.signInBtn').click(function () {
-          $('.Register').addClass('d-none');
-          $('.signIn').removeClass('d-none');
-        });
-  
-  
-      $('.searchMob').click(function(){
-          $('.searchBoxMobile').addClass('active');
-          $('.bottomNavBar').addClass('d-none');
-      });
-      $('.closeBtnSearch').click(function(){
-          $('.searchBoxMobile').removeClass('active');
-          $('.bottomNavBar').removeClass('d-none');
-      });
+
+    // Mobile search box toggle
+    $('.searchMob').click(function () {
+        $('.searchBoxMobile').addClass('active');
+        $('.bottomNavBar').addClass('d-none');
     });
+    $('.closeBtnSearch').click(function () {
+        $('.searchBoxMobile').removeClass('active');
+        $('.bottomNavBar').removeClass('d-none');
+    });
+});
